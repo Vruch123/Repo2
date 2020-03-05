e3c280d688506f31d2ffafa0a7741f9dd8b3909e
veera <veeraiah.cheekati@kritikalvision.ai>
2020-03-05 18:04:53 +0530

    modified

--- HelloJava.java
+++ HelloJava.java
@@ -2,7 +2,7 @@ public class HelloJava {
 	
 	public static void main(String[] args) {
 
-		System.out.println("Hello Java1 new change in master");
+		System.out.println("Hello Java1 now in dev branchtest");
 
 
 		  
